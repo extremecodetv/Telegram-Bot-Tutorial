ca598f6ae21d9652319133a82615c810aa704b34
ExtremeCodeTV <extremecodetv@gmail.com>
2017-03-25 01:08:22 +0700

    Fixed multi reply in HelloCommand

--- Models/Commands/HelloCommand.cs
+++ Models/Commands/HelloCommand.cs
@@ -7,14 +7,14 @@ namespace TelegramBotApp.Models.Commands
     {
         public override string Name => "hello";
 
-        public override async void Execute(Message message, TelegramBotClient client)
+        public override void Execute(Message message, TelegramBotClient client)
         {
             var chatId = message.Chat.Id;
             var messageId = message.MessageId;
 
             //TODO: Command logic -_-
 
-            await client.SendTextMessageAsync(chatId, "Hello!", replyToMessageId: messageId);
+            client.SendTextMessage(chatId, "Hello!", replyToMessageId: messageId);
         }
     }
 }
